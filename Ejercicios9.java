a8a1be93449e237648849f62649fdc10ea65a4dd
Gurrushito <47245652+Gurrushito@users.noreply.github.com>
2019-02-05 14:40:08 +0100

    Update Ejercicios9.java

--- Ejercicios9.java
+++ Ejercicios9.java
@@ -8,10 +8,10 @@ public static void main(String[] args) {
 
 	int numA=0, numB=0, numC=0;
 	
-	System.out.println("Introduce tres números enteros");
+	System.out.println("Introduce tres nÃºmeros enteros");
 	numA=Entrada.entero();
 	numB=Entrada.entero();
-	numC=Entrada.entero();
+	esta mal
 
 		if(numA+numB==numC) {
 			System.out.println("La suma de "+numA+" y "+numB+" es igual a "+numC);
@@ -27,7 +27,7 @@ public static void main(String[] args) {
 					System.out.println("La suma de "+numB+" y "+numC+" es igual a "+numA);
 				}
 				else {
-					System.out.println("Ningún resultado de la suma de dos de los numeros introducidos coinciden con el tercero");
+					System.out.println("NingÃºn resultado de la suma de dos de los numeros introducidos coinciden con el tercero");
 				}
 			}
 		}
